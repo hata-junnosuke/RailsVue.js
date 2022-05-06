4b4e899a908af0085c50385fa860d3b3492429c2
hata-junnosuke <jh19911209@icloud.com>
2022-05-06 22:46:07 +0900

    課題完了

--- app/javascript/pages/PageTimeline.vue
+++ app/javascript/pages/PageTimeline.vue
@@ -2,17 +2,28 @@
     <v-container>
         <v-row>
             <v-col>
-                <div>
+                <div class="pb-10">
                     <div class="mb-5" v-if="$store.getters['auth/currentUser']">
                     <!-- 投稿フォーム -->
                     <!-- $emitからのメソッドは@で受け取り、親の中のメソッド名を設定する -->
                         <micropost-form @created="createMicropost"></micropost-form>
                     </div>
                     <!--投稿リスト  -->
-                    <timeline-list v-if="isExistMicroposts" :microposts="microposts"></timeline-list>
+                    <timeline-list v-if="isExistMicroposts" :microposts="microposts" class="mb-5"></timeline-list>
                     <div class="text-center" v-else>
                         一件もありません
                     </div>
+                    <!-- ページネーション -->
+                    <template v-if="pagingMeta">
+                        <div class="text-center">
+                            <v-pagination
+                                    color="indigo"
+                                    v-model="pagingMeta.current_page"
+                                    :length="pagingMeta.total_pages"
+                                    @input="paging"
+                            ></v-pagination>
+                        </div>
+                    </template>
                 </div>
             </v-col>
         </v-row>
@@ -26,6 +37,8 @@
         data() {
             return {
                 microposts: [],
+                pagingMeta: null,
+                currentPage: 1
             }
         },
         components: {
@@ -45,8 +58,10 @@
         methods: {
             //getでデータを取得
             async fetchMicroposts() {
-                const res = await axios.get(`/api/microposts`)
+                // ページごとに区切りをつける
+                const res = await axios.get(`/api/microposts`, { params: { page: this.currentPage } })
                 this.microposts = res.data.microposts
+                this.pagingMeta = res.data.meta
             },
             //引数で$emitからの値を受け取ることが可能
             async createMicropost(micropostContent) {
@@ -60,6 +75,12 @@
                 const res = await axios.post(`/api/microposts`, micropostParams)
                 // スプレット構文
                 this.microposts =[...[res.data.micropost], ...this.microposts]
+            },
+            // 表示のページ切り替え
+            paging(page) {
+                this.currentPage = page
+                this.fetchMicroposts()
+                this.$vuetify.goTo(0)  //クリックしたときに一番上まで戻ります。
             }
         }
     }
