be46cb73b0bacfc42bf561c8403ad83e405826eb
hata-junnosuke <jh19911209@icloud.com>
2022-05-07 15:31:24 +0900

    課題完了

--- app/javascript/pages/PageUsers.vue
+++ app/javascript/pages/PageUsers.vue
@@ -1,5 +1,35 @@
 <template>
   <v-container>
+    <template>
+      <v-card class="mb-3">
+        <v-card-text>
+          <header>絞り込み条件</header>
+          <v-row dense justify="start">
+            <template>
+              <!-- タグの選択肢を並べる -->
+              <v-checkbox v-for="tag in tags"
+                          :key="tag.id"
+                          v-model="query.selectedTags"
+                          :label="tag.name"
+                          :value="tag.id"
+                          class="mr-5"
+                          @click.native="fetchUsers"
+                          hide-details="auto"
+              >
+              </v-checkbox>
+            </template>
+          </v-row>
+          <v-row>
+            <template>
+              <!-- name検索フォーム -->
+              <v-col cols="12">
+                <v-text-field label="UserName" v-model="query.userName" @input="fetchUsers"></v-text-field>
+              </v-col>
+            </template>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </template>
     <v-row dense>
       <v-col
               v-for="user in users"
@@ -10,27 +40,46 @@
         <v-card>
           <v-img
                   :src="`http://placeimg.com/300/300/people?dummy=${user.id}`"
+                  :lazy-src="`https://picsum.photos/10/6?dummy=${user.id}`"
                   class="white--text align-end"
                   gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  aspect-ratio="1"
           >
-            <v-card-title v-text="user.name"></v-card-title>
+            <template v-slot:placeholder>
+              <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+              >
+                <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+              </v-row>
+            </template>
+            <!-- 名前をクリックで詳細へ -->
+            <v-card-title v-text="user.name" @click="$router.push(`/users/${user.id}`)" style="cursor: pointer;"></v-card-title>
           </v-img>
 
-          <v-card-text class="text--primary">
-            <div>#Ruby</div>
+          <!-- タグの表示 -->
+          <v-card-text class="text--primary" style="min-height: 64px;">
+            <v-chip
+                    class="ma-1"
+                    color="orange"
+                    text-color="white"
+                    small
+                    v-for="tag in user.tags" :key="tag.name"
+            >
+              <v-icon left class="mr-0">mdi-music-accidental-sharp</v-icon>
+              {{tag.name}}
+            </v-chip>
           </v-card-text>
 
           <v-card-actions>
             <v-spacer></v-spacer>
-
             <v-btn icon>
               <v-icon>mdi-heart</v-icon>
             </v-btn>
-
             <v-btn icon>
               <v-icon>mdi-bookmark</v-icon>
             </v-btn>
-
             <v-btn icon>
               <v-icon>mdi-share-variant</v-icon>
             </v-btn>
@@ -38,24 +87,69 @@
         </v-card>
       </v-col>
     </v-row>
+    <v-row justify="center">
+      <template v-if="pagingMeta">
+        <div class="text-center">
+          <v-pagination
+                  color="indigo"
+                  v-model="pagingMeta.current_page"
+                  :length="pagingMeta.total_pages"
+                  @input="paging"
+          ></v-pagination>
+        </div>
+      </template>
+    </v-row>
   </v-container>
 </template>
 
 <script>
   import axios from 'axios'
+  import qs from 'qs';
   export default {
       data() {
           return {
-              users: []
+              users: [],
+              tags: [],
+              query: {
+                  selectedTags: [],
+                  userName: ''
+              },
+              pagingMeta: null,
+              currentPage: 1
           }
       },
       created() {
           this.fetchUsers()
+          this.fetchTags()
       },
       methods: {
+          // コメントアウトでタグidを1にチェックした時の処理を記録
           async fetchUsers() {
-              const res = await axios.get(`/api/users`)
+              // フォームに入れた値をsearchParamsへ格納
+              const searchParams = {
+                  q: {
+                      name: this.query.userName,
+                      tag_ids: this.query.selectedTags
+                  }
+              }
+              const pagingParams = { page: this.currentPage }
+              const params = { ...searchParams, ...pagingParams }
+              // params = {q:{name: "", tag_ids: [1]},page: 1}
+              const paramsSerializer = (params) => qs.stringify(params, { arrayFormat: 'brackets' });
+              // qs.stringify(params, { arrayFormat: 'brackets' }) = q%5Bname%5D=&q%5Btag_ids%5D%5B%5D=1&page=1
+              const res = await axios.get(`/api/users`, { params, paramsSerializer })//URLにがっちゃんこ
               this.users = res.data.users
+              this.pagingMeta = res.data.meta
+          },
+          // タグの選択肢を作るために取得
+          async fetchTags() {
+              const res = await axios.get(`/api/tags`)
+              this.tags = res.data.tags
+          },
+          paging(page) {
+              this.currentPage = page
+              this.fetchUsers()
+              this.$vuetify.goTo(0)
           }
       }
   }
