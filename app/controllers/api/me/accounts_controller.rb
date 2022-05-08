ce1eac78975432b5f4148903586dd7a098e40d74
hata-junnosuke <jh19911209@icloud.com>
2022-05-08 11:47:57 +0900

    課題完了

--- app/controllers/api/me/accounts_controller.rb
+++ app/controllers/api/me/accounts_controller.rb
@@ -2,7 +2,8 @@ class Api::Me::AccountsController < ApplicationController
   before_action :authenticate
 
   def update
-    current_user.update(user_params)
+    current_user.assign_attributes(user_params) #　配列でuser_paramsを受け取り設定（保存はまだ）
+    current_user.save_with_tags!(tag_names: tag_names) # タグが既に存在する場合は既存のレコードを取得、なければ新規作成
     render json: current_user, serializer: UserSerializer
   end
 
@@ -10,5 +11,10 @@ class Api::Me::AccountsController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :avatar, avatar: :data)
+  end
+
+  def tag_names
+    # ユーザーのタグを参照する
+    params.dig(:user, :tag_names)
   end
 end
